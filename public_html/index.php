df6934bd3857fff0b47262a46e3be94dd0c347de
Chugalug -- Linux User'S Group <chugalug@www-c1-mgmt.cc.uga.edu>
2016-07-05 23:33:44 -0400

    Updated for July 2016

--- public_html/index.php
+++ public_html/index.php
@@ -6,15 +6,15 @@ Meetings occur on the first Wednesday of each month from 8-9:30 PM and are held 
 If you would like to help out during Free IT Athens' open hours (great fun and experience introducing new people to Linux!), you need only attend their very helpful and low-stress new volunteer orientation.  <a href="http://www.freeitathens.org/get_involved/">Click here to learn more about becoming a volunteer at Free IT Athens</a>.
 
 <h3>This Month's Meeting</h3>
-<b>Event Planning</b>
+<b>Athens LinuxFest!</b>
 </br>
-Wed., 1 June., 2016, 8:00 PM
+Wed., 5 July., 2016, 6 - 9:30 PM
 </br>
 Everyone is welcome!
 <br></br>
-During this meeting, we'll be planning the 6 July InstallFest.  Come on by and help!
+Come on out!  <a href="/fest">Click here to learn more!</a>
 <br></br>
-Frita open hours are from 6-8 PM. Feel free to show up in that window if you want to check them out. The meeting opens at 8 PM, but actually begins at 8:15 (this allows the Frita staff to close down from open hours).  If there is a presentation, it will begin promptly at 8:15 and should take about an hour.  Questions during the presentation are encouraged.  The meeting ends at 9:30.
+General notes about meetings:  Frita open hours are from 6-8 PM. Feel free to show up in that window if you want to check them out. The meeting opens at 8 PM, but actually begins at 8:15 (this allows the Frita staff to close down from open hours).  If there is a presentation, it will begin promptly at 8:15 and should take about an hour.  Questions during the presentation are encouraged.  The meeting ends at 9:30.
 <br></br>
 </td>
 
---
+++ public_html/meetings/2016-06-01.php
@@ -0,0 +1,15 @@
+<?php include dirname(__FILE__).'/../header.php';?>
+
+<div style="width:520px;">
+<br></br>
+<h2>Planning</h2> 
+A few folks</br>
+<p>Date: Jun. 01, 2016</p>
+</br>
+We worked on planning the 6 July LinuxFest.  
+<br></br>
+</br>
+</td>
+</div>
+
+<?php include dirname(__FILE__).'/../footer.php';?>
