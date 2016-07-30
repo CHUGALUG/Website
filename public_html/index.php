cb4929e7c02082d4fa53a06ff2dd3ec9f1c4533e
Chugalug -- Linux User'S Group <chugalug@www-c1-mgmt.cc.uga.edu>
2016-07-29 23:29:33 -0400

    BLF:  setting info for this month's meeting

--- public_html/index.php
+++ public_html/index.php
@@ -1,20 +1,28 @@
 <?php include 'header.php';?>
 
 <h3>Meetings</h3>
-Meetings occur on the first Wednesday of each month from 8-9:30 PM and are held at <a href="/map.php">Free IT Athens</a>.  Please join the Facebook group (link above) or the Mailing list (link at left in sidebar) if you would like to join discussions about the meetings.
+Meetings occur on the first Wednesday of each month from 8-9:30 PM and are held at <a href="/map.php">Free IT Athens</a>.  Please join the Facebook group (link above) or the Mailing list (link at left in sidebar) if you would like to join discussions about the meetings.  See below for general notes about the meetings.
 <br></br>
 If you would like to help out during Free IT Athens' open hours (great fun and experience introducing new people to Linux!), you need only attend their very helpful and low-stress new volunteer orientation.  <a href="http://www.freeitathens.org/get_involved/">Click here to learn more about becoming a volunteer at Free IT Athens</a>.
 
 <h3>This Month's Meeting</h3>
-<b>Athens LinuxFest!</b>
+<b>Regular Meeting</b>
 </br>
-Wed., 5 July., 2016, 6 - 9:30 PM
+Wed., 3 August., 2016, 6 - 9:30 PM
 </br>
-Everyone is welcome!
+Everyone!
 <br></br>
-Come on out!  <a href="/fest">Click here to learn more!</a>
+Come on out to chat about Linux!
 <br></br>
-General notes about meetings:  Frita open hours are from 6-8 PM. Feel free to show up in that window if you want to check them out. The meeting opens at 8 PM, but actually begins at 8:15 (this allows the Frita staff to close down from open hours).  If there is a presentation, it will begin promptly at 8:15 and should take about an hour.  Questions during the presentation are encouraged.  The meeting ends at 9:30.
+<h3>This month's Noob Hour at Free I. T. Athens:</h3>
+<b>VirtualBox</b>
+</br>
+7 PM, same day.
+</br>
+<a href="https://www.facebook.com/events/204529739949692/">Click here to learn more!</a>
+<br></br>
+<h3>General notes about meetings</h3>
+Frita open hours are from 6-8 PM. Feel free to show up in that window if you want to check them out. The meeting opens at 8 PM, but actually begins at 8:15 (this allows the Frita staff to close down from open hours).  If there is a presentation, it will begin promptly at 8:15 and should take about an hour.  Questions during the presentation are encouraged.  The meeting ends at 9:30.
 <br></br>
 </td>
 
---
+++ public_html/meetings/2016-07-05.php
@@ -0,0 +1,18 @@
+<?php include dirname(__FILE__).'/../header.php';?>
+
+<div style="width:520px;">
+<br></br>
+<h2>Athens LinuxFest!</h2> 
+A joint CHUGALUG-FRITA event</br>
+<p>Date: Jul. 05, 2016, 6-9:30 PM</p>
+</br>
+<a href="/fests/2016-07-06.php">Click here to view the announcement.</a>
+</br>
+It was great fun!  There was lots of talking about GNU and Linux
+and installing and trying things out and watching videos.  Also food.
+<br></br>
+</br>
+</td>
+</div>
+
+<?php include dirname(__FILE__).'/../footer.php';?>
