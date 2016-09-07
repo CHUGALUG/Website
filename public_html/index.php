3514166c8f9358e22faa346355042dda217ce2e1
Chugalug -- Linux User'S Group <chugalug@www-c1-mgmt.cc.uga.edu>
2016-09-07 01:17:00 -0400

    update site for Sept meeting

--- public_html/index.php
+++ public_html/index.php
@@ -8,18 +8,11 @@ If you would like to help out during Free IT Athens' open hours (great fun and e
 <h3>This Month's Meeting</h3>
 <b>Regular Meeting</b>
 </br>
-Wed., 3 August., 2016, 6 - 9:30 PM
+Wed., 7 September., 2016, 8 - 9:30 PM
 </br>
 Everyone!
 <br></br>
 Come on out to chat about Linux!
-<br></br>
-<h3>This month's Noob Hour at Free I. T. Athens:</h3>
-<b>VirtualBox</b>
-</br>
-7 PM, same day.
-</br>
-<a href="https://www.facebook.com/events/204529739949692/">Click here to learn more!</a>
 <br></br>
 <h3>General notes about meetings</h3>
 Frita open hours are from 6-8 PM. Feel free to show up in that window if you want to check them out. The meeting opens at 8 PM, but actually begins at 8:15 (this allows the Frita staff to close down from open hours).  If there is a presentation, it will begin promptly at 8:15 and should take about an hour.  Questions during the presentation are encouraged.  The meeting ends at 9:30.
---
+++ public_html/meetings/2016-08-03.php
@@ -0,0 +1,18 @@
+<?php include dirname(__FILE__).'/../header.php';?>
+
+<div style="width:520px;">
+<br></br>
+<h2>Regular Meeting</h2> 
+Everyone!</br>
+<p>Date: Aug. 08, 2016, 8-9:30 PM</p>
+</br>
+We mostly chatted about the next LinuxFest.
+<br></br>
+There was also a 
+<a href="https://www.facebook.com/events/204529739949692/">Noob Hour about VirtualBox</a>
+ before the meeting at 7 PM.
+</br>
+</td>
+</div>
+
+<?php include dirname(__FILE__).'/../footer.php';?>
