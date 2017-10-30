800a5c07bad1943dff595bdef3eb0e5b53122b48
Chugalug -- Linux User'S Group <chugalug@ld-stnd-mgmt01.dev.uga.edu>
2017-10-30 06:53:55 -0400

    updating for November

--- public_html/index.php
+++ public_html/index.php
@@ -8,30 +8,13 @@ Frita Noob-Hour presentations sometimes occur a week or two before the CHUGALUG 
 If you would like to help out during Free IT Athens' open hours (great fun and experience introducing new people to Linux!), you need only attend their very helpful and low-stress new volunteer orientation.  <a href="http://www.freeitathens.org/get_involved/">Click here to learn more about becoming a volunteer at Free IT Athens</a>.
 
 <h3>This Month's Meeting</h3>
-<b>Regular Meeting - Key-Signing Party!</b>
+<b>Regular Meeting - No Special Topic</b>
 <br></br>
-Wed., 2 August, 2017, 8 - 9:30 PM
+Wed., 1 November, 2017, 8 - 9:30 PM
 </br>
 Everyone!
 <br></br>
-Come on out to chat about encryption and privacy and to get your keys signed!
-Here are some sites with useful info about that:<br>
-<a href="https://en.wikipedia.org/wiki/Key_signing_party">
-https://en.wikipedia.org/wiki/Key_signing_party</a><br>
-<a href="http://www.cryptnet.net/fdp/crypto/keysigning_party/en/keysigning_party.html">
-http://www.cryptnet.net/fdp/crypto/keysigning_party/en/keysigning_party.html</a><br>
-<a href="https://herrons.com/keysigning-party-guide/">
-https://herrons.com/keysigning-party-guide/</a><br>
-<a href="https://wiki.ubuntu.com/KeySigningParty">
-https://wiki.ubuntu.com/KeySigningParty</a><br>
-<a href="https://wiki.debian.org/Keysigning">
-https://wiki.debian.org/Keysigning</a><br>
-<a href="https://logological.org/keysigning">
-https://logological.org/keysigning</a><br>
-<a href="http://rhonda.deb.at/projects/gpg-party/gpg-party.en.html">
-http://rhonda.deb.at/projects/gpg-party/gpg-party.en.html</a><br>
-<a href="https://xkcd.com/364/">
-https://xkcd.com/364/</a>
+Come on out to chat about Linux!
 <br></br>
 <h3>General notes about meetings</h3>
 Frita open hours are from 6-8 PM. Feel free to show up in that window if you want to check them out. The meeting opens at 8 PM, but actually begins at 8:15 (this allows the Frita staff to close down from open hours).  If there is a presentation, it will begin promptly at 8:15 and should take about an hour.  Questions during the presentation are encouraged.  The meeting ends at 9:30.
