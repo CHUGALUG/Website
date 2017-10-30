8bcc6a36282f827f8c00fe8c5cfca1493ba99a13
Chugalug -- Linux User'S Group <chugalug@lp-stnd-mgmt01.cc.uga.edu>
2017-10-30 06:56:41 -0400

    there wasn't git on the prod server for a while, so things happened strangely

---
+++ public_html/meetings/2017-05-03.php
@@ -0,0 +1,15 @@
+<?php include dirname(__FILE__).'/../header.php';?>
+
+<div style="width:520px;">
+<br></br>
+<h2>Regular Meeting</h2> 
+Everyone!</br>
+<p>Date: May 03, 2017, 8-9:30 PM</p>
+</br>
+Mostly, we chatted about why more people don't use Linux.
+<br></br>
+</br>
+</td>
+</div>
+
+<?php include dirname(__FILE__).'/../footer.php';?>
---
+++ public_html/old/fourth_tuesdays_four_athens_index_until_2015-09-25.php
@@ -0,0 +1,26 @@
+<?php include 'header.php';?>
+
+<h3>Meetings</h3>
+<ul>
+<li>Meetings are held on fourth Tuesdays at 7:00 PM at <a href="/map.php">Four Athens</a>.</li>
+<li>When possible, meetings might also be <a href="http://www.ustream.tv/channel/chugalug">streamed live over Ustream.</a></li>
+<li>There are two presentations:  introductory and advanced.</li>
+</ul>
+<h3>This Month's Meeting</h3>
+Tuesday, 28 Apr, 2015 at 7 PM
+</br>
+Presentation room at <a href="/map.php">Four Athens</a>
+</br>
+<a href="http://chugalug.uga.edu/graphics/Meeting_flyer_Zabbix_2015-04-28.png">Meeting Flyer</a>
+<br></br>
+<b> <a href="http://www.zabbix.com">Zabbix:</a> All the monitoring</b> 
+<br>David Millians</br>
+</br>
+<u>Introductory and Advanced:</u>
+<br></br>
+You can't check every computer you have all the time, but you can set a computer to do it for you. If you need to know NOW when something is wrong, Zabbix's blend of GUI and details may be right for you.
+</br>
+</td>
+
+
+<?php include 'footer.php';?>
