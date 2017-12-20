8aeac542b1727fa4e53c17d01734329dd2772e0a
Chugalug -- Linux User'S Group <chugalug@ld-stnd-mgmt01.dev.uga.edu>
2017-12-20 10:40:54 -0500

    updating for the Jan 2018 meeting

---
+++ public_html/meetings/2017-12-06.php
@@ -0,0 +1,17 @@
+<?php include dirname(__FILE__).'/../header.php';?>
+
+<div style="width:520px;">
+<br></br>
+<h2>Regular Meeting</h2> 
+Everyone!</br>
+<p>Date: Dec. 06, 2017, 8-9:30 PM</p>
+</br>
+This meeting was originally cancelled. At the last minute, it was
+un-cancelled.  But, due to the late notice, it was not a 
+well-attended meeting. 
+<br></br>
+</br>
+</td>
+</div>
+
+<?php include dirname(__FILE__).'/../footer.php';?>
