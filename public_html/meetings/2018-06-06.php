5c1997be76fd90fcb177c4b0deea048e8e246c09
Bob Belnap <robert.belnap@uga.edu>
2018-06-07 15:58:51 -0400

    add info about ssh part two meeting

---
+++ public_html/meetings/2018-06-06.php
@@ -0,0 +1,40 @@
+<?php include dirname(__FILE__).'/../header.php';?>
+
+<div style="width:520px;">
+<br></br>
+<h2>SSH Part two</h2> 
+Everyone!</br>
+<p>Date: Jun. 06, 2018, 8-9:30 PM</p>
+</br>
+Unfortunately, the youtube livesteam video is without sound, so is unavailable.  For reference, the slides used can be found at:
+</br>
+<p>
+<a href="http://burninate.org/ssh/">http://burninate.org/ssh/</a>
+</br>
+</p>
+
+The link about port forwarding is:  <a href="https://solitum.net/an-illustrated-guide-to-ssh-tunnels/">https://solitum.net/an-illustrated-guide-to-ssh-tunnels/</a></br>
+
+<p>
+Controlmaster multiplexing link is: <a href="https://en.wikibooks.org/wiki/OpenSSH/Cookbook/Multiplexing">https://en.wikibooks.org/wiki/OpenSSH/Cookbook/Multiplexing</a></br>
+</p>
+
+
+<p>
+The additional software links are: </br></br>
+</p>
+
+<p>
+ansible - <a href="https://www.ansible.com/">https://www.ansible.com/</a></br>
+fabric - <a href="http://www.fabfile.org/">http://www.fabfile.org/</a></br>
+sshfs - <a href="https://github.com/libfuse/sshfs">https://github.com/libfuse/sshfs</a></br>
+sshuttle - <a href="https://github.com/sshuttle/sshuttle">https://github.com/sshuttle/sshuttle</a></br>
+</p>
+
+
+<br></br>
+</br>
+</td>
+</div>
+
+<?php include dirname(__FILE__).'/../footer.php';?>
